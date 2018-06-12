0445d9b22aa450b3aa4b357806a39fb71c680d48
Jérémie Dumas <jeremie.dumas@ens-lyon.org>
2018-06-12 00:14:49 -0400

    Experiment with setting DOWNLOAD_DIR.

--- shared/cmake/prerequisites.cmake
+++ shared/cmake/prerequisites.cmake
@@ -1,10 +1,11 @@
 include(DownloadProject)
 
 download_project(
-	PROJ       eigen
-	URL        http://bitbucket.org/eigen/eigen/get/3.2.10.tar.gz
-	URL_MD5    8ad10ac703a78143a4062c9bda9d8fd3
-	SOURCE_DIR ${LIBIGL_EXTERNAL}/eigen
+	PROJ         eigen
+	URL          http://bitbucket.org/eigen/eigen/get/3.2.10.tar.gz
+	URL_MD5      8ad10ac703a78143a4062c9bda9d8fd3
+	SOURCE_DIR   ${LIBIGL_EXTERNAL}/eigen
+	DOWNLOAD_DIR ${LIBIGL_EXTERNAL}/.cache/eigen
 )
 
 if(LIBIGL_WITH_EMBREE)
@@ -12,10 +13,11 @@ if(LIBIGL_WITH_EMBREE)
 	# Note that if we use the URL of the release directly, we don't have to download the .git folder (saves 70 MB)
 	download_project(
 		PROJ           embree
-		URL            https://github.com/embree/embree/archive/v2.17.4.tar.gz
-		URL_MD5        2038f3216b1d626e87453aee72c470e5
-		# GIT_REPOSITORY https://github.com/embree/embree.git
-		# GIT_TAG        v2.17.4
+		# URL            https://github.com/embree/embree/archive/v2.17.4.tar.gz
+		# URL_MD5        2038f3216b1d626e87453aee72c470e5
+		GIT_REPOSITORY https://github.com/embree/embree.git
+		GIT_TAG        cb61322db3bb7082caed21913ad14869b436fe78
 		SOURCE_DIR     ${LIBIGL_EXTERNAL}/embree
+		DOWNLOAD_DIR   ${LIBIGL_EXTERNAL}/.cache/embree
 	)
 endif()
